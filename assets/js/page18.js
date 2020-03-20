b13763e2ee8832b19608fcf3bdc1d81452e65460
duynguyenfpt <duyntse04704@fpt.edu.vn>
2020-03-20 17:28:45 +0700

    Page19-28
    Including Modal

---
+++ assets/js/page18.js
@@ -0,0 +1,5 @@
+// $('#modalP18').on('shown.bs.modal', function () {
+//     $('html').css('overflow', 'hidden');
+// }).on('hidden.bs.modal', function () {
+//     $('html').css('overflow', 'auto');
+// });
