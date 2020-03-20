f39c4ed1140059004dd568fb7671b41465db7aba
Quoc Tuan <quoctuanck99@outlook.com>
2020-03-20 19:48:24 +0700

    25,26

--- assets/js/page27Animation.js
+++ assets/js/page27Animation.js
@@ -2,10 +2,11 @@ $('#carouselExampleControls').bind('slid.bs.carousel', function(e) {
     var ele = $('#carouselExampleControls .carousel-indicators li.active');
     var pageIndex = ele.data('value');
     if (pageIndex == 26) {
-        diagonalMove("#speakerPage27", 100, 200, 200)
+        speakerMove = diagonalMove("#speakerPage27", 200, 1000, 200, 200)
+        speakerMove.play();
         toXDelay("#cardPage27", 200, -600, -600)
         appear("#cardTitlePage27", 2000)
-        appearDelay("#cardContentPage27", 1000, 1500)
+        appearDelay("#cardContentPage27", 1000, 1000)
     } else {
         diagonalMove("#speakerPage27", 0, -200, -200)
         returnX("#cardPage27", 200, 600, 600)
@@ -14,13 +15,13 @@ $('#carouselExampleControls').bind('slid.bs.carousel', function(e) {
 });
 
 ///////////////////////////////////////////toleft delay///////////////////////////////////////
-function diagonalMove(obj, time, x, y) {
-    objects = anime({
+function diagonalMove(obj, delay, duration, x, y) {
+    return objects = anime({
         targets: obj,
-        delay: time,
-        opacity: [{ value: 1, duration: time }],
+        delay: delay,
+        opacity: [{ value: 1, duration: duration }],
         translateX: x,
         translateY: y,
+        duration: duration
     })
-    objects.play();
 }
