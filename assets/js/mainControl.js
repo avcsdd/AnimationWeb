b2f7ecf42f69a7ab4975b93ed66d1665ea5e3f24
duynguyenfpt <39914280+duynguyenfpt@users.noreply.github.com>
2020-03-23 11:31:43 +0700

    restructure project
    Add architect load optimization
    Add new animation folders

---
+++ assets/js/mainControl.js
@@ -0,0 +1,44 @@
+$("#textAboveNav").hide();
+const mapPage = new Map([
+    [0, 'page01'],
+    [1, 'page01'],
+    [2, 'page01'],
+    [3, 'page02'],
+    [4, 'page02'],
+    [5, 'page02'],
+    [6, 'page02'],
+    [12, 'page02'],
+    [13, 'page02'],
+    [14, 'page03'],
+    [15, 'page04'],
+    [16, 'page04'],
+    [17, 'page04'],
+    [24, 'page05'],
+    [25, 'page05'],
+    [26, 'page05'],
+    [27, 'page06'],
+    [28, 'page06'],
+    [29, 'page07'],
+    [30, 'page08'],
+]);
+
+$(document).ready(function(){
+    $('#carouselExampleControls').on('slid.bs.carousel', function(e) {
+        var ele = $('#carouselExampleControls .carousel-indicators li.active');
+        var pageIndex = ele.data('value');
+        //
+        let id = mapPage.get(pageIndex);
+        $(".nav-item").removeClass("active");
+        $("#" + id).addClass("active");
+        if (pageIndex == 0) {
+            $("#textAboveNav").hide();
+        } else {
+            $("#textAboveNav").show();
+        }
+        //
+        if (pageIndex==14){
+            // page15Animation.boxTextP15.play()
+            // Add(2,3)
+        }
+    })
+})
