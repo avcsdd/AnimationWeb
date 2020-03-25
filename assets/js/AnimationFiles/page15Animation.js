611244562d69149ad49f8f330fb59779059dba0c
Quoc Tuan <quoctuanck99@outlook.com>
2020-03-25 14:04:55 +0700

    UPDATE ANIMATION

--- assets/js/AnimationFiles/page15Animation.js
+++ assets/js/AnimationFiles/page15Animation.js
@@ -1,12 +1,22 @@
 var ani = new MyAnimation();
 class Page15nimation {
-    boxTextP15 = ani.moveHorizontalDelay("#boxTextP15", 500, 1000, 200)
+    boxTextP15 = ani.appearDelay("#boxTextP15", 500, 1000, 200)
     note1P15 = ani.appearDelay("#note1P15", 1000, 1500)
     note2P15 = ani.appearDelay("#note2P15", 1000, 2000)
     note3P15 = ani.appearDelay("#note3P15", 1000, 2500)
     note4P15 = ani.appearDelay("#note4P15", 1000, 3000)
     note5P15 = ani.appearDelay("#note5P15", 1000, 3500)
     note6P15 = ani.appearDelay("#note6P15", 1000, 4000)
+    boxtexP15Shaking = anime({
+        targets: "#boxTextP15",
+        // translateX: { value: 5, delay: 0 },
+        rotateX: '0.02turn',
+        direction: 'alternate',
+        loop: true,
+        // delay: 0,
+        autoplay: false,
+        easing: 'easeInOutQuad'
+    })
     run() {
         this.boxTextP15.restart();
         this.note1P15.restart();
@@ -15,6 +25,7 @@ class Page15nimation {
         this.note4P15.restart();
         this.note5P15.restart();
         this.note6P15.restart();
+        this.boxtexP15Shaking.restart()
     }
     reverse() {
 
--- assets/js/AnimationFiles/page16Animation.js
+++ assets/js/AnimationFiles/page16Animation.js
@@ -1,14 +1,23 @@
 var ani = new MyAnimation();
 class AnimationPage16 {
     policeP16 = ani.moveVerticalDelay("#policeP16", 500, 1500, 80)
-    leftTextBoxP16 = ani.moveHorizontalDelay("#leftTextBoxP16", 1500, 1000, 70)
-    rightTextBoxP16 = ani.moveHorizontalDelay("#rightTextBoxP16", 1500, 1000, -70)
-    bottomTextBoxP16 = ani.moveVerticalDelay("#bottomTextBoxP16", 1500, 1000, -50)
+
+    // rightTextBoxP16 = ani.moveHorizontalDelay("#rightTextBoxP16", 1500, 1000, -70)
+    // leftTextBoxP16 = ani.moveHorizontalDelay("#leftTextBoxP16", 2000, 1000, 70)
+    // bottomTextBoxP16 = ani.moveVerticalDelay("#bottomTextBoxP16", 2500, 1000, -50)
+    rightTextBoxP16 = ani.appearDelay("#rightTextBoxP16", 1000, 1500)
+    leftTextBoxP16 = ani.appearDelay("#leftTextBoxP16", 1000, 2000)
+    bottomTextBoxP16 = ani.appearDelay("#bottomTextBoxP16", 1000, 2500)
+        // rightTextBoxP16UD0 = ani.moveHorizontalDelay("#rightTextBoxP16", 3000, 0, 70)
+    rightTextBoxP16UD = ani.moveEasingUD(["#rightTextBoxP16", "#leftTextBoxP16", "#bottomTextBoxP16"], 5000, 10)
     run() {
         this.policeP16.restart();
         this.leftTextBoxP16.restart();
         this.rightTextBoxP16.restart();
         this.bottomTextBoxP16.restart();
+        // this.rightTextBoxP16UD0.restart()
+        this.rightTextBoxP16UD.restart();
+
     }
     reverse() {
 
