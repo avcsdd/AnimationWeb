d4ed9d0ac7b15c44dca4e086c17bf1e23a196785
duynguyenfpt <39914280+duynguyenfpt@users.noreply.github.com>
2020-04-23 14:44:05 +0700

    Update page15Animation.js

--- assets/js/AnimationFiles/page15Animation.js
+++ assets/js/AnimationFiles/page15Animation.js
@@ -1,17 +1,17 @@
 var ani = new MyAnimation();
 class Page15nimation {
     // boxTextP15 = ani.moveHorizontalDelay("#boxTextP15", 500, 1000, 20)
-    line1P15 = ani.appearDelay("#first-line", 500, 500)
-    line2P15 = ani.appearDelay("#second-line", 500, 1000)
-    line3P15 = ani.appearDelay("#third-line", 500, 1500)
-    line4P15 = ani.appearDelay("#forth-line", 500, 2000)
-    line5P15 = ani.appearDelay("#fifth-line", 500, 2500)
-    note1P15 = ani.appearDelay("#note1P15", 500, 3000)
-    note2P15 = ani.appearDelay("#note2P15", 500, 3500)
-    note3P15 = ani.appearDelay("#note3P15", 500, 4000)
-    note4P15 = ani.appearDelay("#note4P15", 500, 4500)
-    note5P15 = ani.appearDelay("#note5P15", 500, 5000)
-    note6P15 = ani.appearDelay("#note6P15", 500, 5500)
+    line1P15 = ani.appearDelay("#first-line", 500, 800)
+    line2P15 = ani.appearDelay("#second-line", 500, 1600)
+    line3P15 = ani.appearDelay("#third-line", 500, 2400)
+    line4P15 = ani.appearDelay("#forth-line", 500, 3200)
+    line5P15 = ani.appearDelay("#fifth-line", 500, 4000)
+    note1P15 = ani.appearDelay("#note1P15", 500, 4500)
+    note2P15 = ani.appearDelay("#note2P15", 500, 5000)
+    note3P15 = ani.appearDelay("#note3P15", 500, 5500)
+    note4P15 = ani.appearDelay("#note4P15", 500, 6000)
+    note5P15 = ani.appearDelay("#note5P15", 500, 6500)
+    note6P15 = ani.appearDelay("#note6P15", 500, 7000)
         // boxtexP15Shaking = anime({
         //     targets: "#boxTextP15",
         //     // translateX: { value: 5, delay: 0 },
